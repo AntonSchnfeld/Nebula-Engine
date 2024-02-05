0dfef2dba13584d2edb2b4daf4e9ee0f25309105
SchnfeldDev <schnfeldanton@gmail.com>
2024-02-05 22:57:26 +0100

    Added methods for reading Java resources

--- Core/src/main/java/org/nebula/io/Files.java
+++ Core/src/main/java/org/nebula/io/Files.java
@@ -1,42 +1,21 @@
 package org.nebula.io;
 
 import org.lwjgl.BufferUtils;
+import org.lwjgl.system.MemoryStack;
+import org.lwjgl.system.MemoryUtil;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.net.URL;
 import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
 import java.nio.file.Path;
+import java.util.MissingResourceException;
+import java.util.Objects;
 
 import static org.lwjgl.stb.STBImage.*;
 
 public class Files
 {
-    public static ByteBufferedImage readImageFromResource(String resourcePath) {
-        IntBuffer width = BufferUtils.createIntBuffer(1);
-        IntBuffer height = BufferUtils.createIntBuffer(1);
-        IntBuffer channels = BufferUtils.createIntBuffer(1);
-
-        byte[] byteArr = null;
-
-        try (InputStream in = Files.class.getResourceAsStream(resourcePath)) {
-            if (in != null) byteArr = in.readAllBytes();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        if (byteArr == null)
-            throw new RuntimeException(new FileNotFoundException("Could not read resource: " + resourcePath));
-
-        ByteBuffer byteBuffer = BufferUtils.createByteBuffer(byteArr.length);
-        byteBuffer.put(byteArr, 0, byteArr.length);
-
-        ByteBuffer image = stbi_load_from_memory(byteBuffer, width, height, channels, STBI_default);
-
-        return new ByteBufferedImage(image, width.get(), height.get(), channels.get());
-    }
-
     public static ByteBufferedImage readImage(String filePath)
     {
         IntBuffer width = BufferUtils.createIntBuffer(1);
@@ -51,7 +30,7 @@ public class Files
         return new ByteBufferedImage(bytes, width.get(), height.get(), channels.get());
     }
 
-    public static byte[] readFile (String filePath) {
+    public static byte[] readFile(String filePath) {
         try {
             return java.nio.file.Files.readAllBytes(Path.of(filePath));
         } catch (IOException e) {
@@ -59,7 +38,53 @@ public class Files
         }
     }
 
-    public static String readFileAsString (String filePath) {
+    public static String readFileAsString(String filePath) {
         return new String(readFile(filePath));
+    }
+
+    public static ByteBufferedImage readImageFromResource(String resourceName) {
+        IntBuffer width = BufferUtils.createIntBuffer(1);
+        IntBuffer height = BufferUtils.createIntBuffer(1);
+        IntBuffer channels = BufferUtils.createIntBuffer(1);
+
+        byte[] byteArr = readResource(resourceName);
+
+        ByteBuffer byteBuffer = BufferUtils.createByteBuffer(byteArr.length);
+        byteBuffer.put(byteArr, 0, byteArr.length);
+        byteBuffer.flip();
+
+        ByteBuffer image = stbi_load_from_memory(byteBuffer, width, height, channels, STBI_default);
+        if (image == null)
+            throw new RuntimeException("STB Failure reason: " + stbi_failure_reason());
+
+        ByteBufferedImage bbi = new ByteBufferedImage(image, width.get(), height.get(), channels.get());
+
+        MemoryUtil.memFree(width);
+        MemoryUtil.memFree(height);
+        MemoryUtil.memFree(channels);
+
+        return bbi;
+    }
+
+    public static byte[] readResource(String resourceName) {
+        URL url = ClassLoader.getSystemClassLoader().getResource(resourceName);
+        Objects.requireNonNull(url);
+
+        byte[] bytes = null;
+
+        try (BufferedInputStream in = new BufferedInputStream(url.openStream())) {
+            bytes = in.readAllBytes();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        if (bytes == null)
+            throw new RuntimeException(new FileNotFoundException("Could not read resource: " + resourceName));
+
+        return bytes;
+    }
+
+    public static String readResourceAsString(String resourceName) {
+        return new String(readResource(resourceName));
     }
 }

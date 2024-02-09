b6cf97b26fe7894c3bc6cc485600694e8cf53b27
schoenfelddev <schnfeldanton@gmail.com>
2024-02-09 17:50:00 +0100

    Added Camera and Texture to MeshTest and used more complex shaders

--- JGL/src/test/java/org/nebula/MeshTest.java
+++ JGL/src/test/java/org/nebula/MeshTest.java
@@ -1,10 +1,13 @@
 package org.nebula;
 
+import org.joml.Vector2f;
 import org.joml.Vector2i;
 import org.nebula.io.Files;
+import org.nebula.jgl.Camera;
 import org.nebula.jgl.data.buffer.Buffer;
 import org.nebula.jgl.data.buffer.Mesh;
 import org.nebula.jgl.data.shader.Shader;
+import org.nebula.jgl.data.texture.Texture;
 import org.nebula.jglfw.GLFWWindow;
 
 import static org.lwjgl.opengl.GL11C.*;
@@ -13,6 +16,8 @@ public class MeshTest {
     private Mesh mesh;
     private Shader shader;
     private GLFWWindow window;
+    private Camera camera;
+    private Texture texture;
     private Vector2i windowSize;
 
     public MeshTest() {
@@ -21,22 +26,34 @@ public class MeshTest {
         window.createGLCapabilities();
         windowSize = new Vector2i();
 
-        shader = new Shader(Files.readResourceAsString("triangles.vert"),
-                Files.readResourceAsString("triangles.frag"));
+        float size = 3;
+        camera = new Camera(new Vector2f(0, 0), -size, size, -size, size, -size, size);
+
+        shader = new Shader(Files.readResourceAsString("default.vert"),
+                Files.readResourceAsString("default.frag"));
+        texture = new Texture(Files.readImageFromResource("nebula.png"), true);
         float[] vertices = {
-                -0.75f, -0.75f, 1, 1, 1, 1,
-                0.75f, -0.75f, 1, 1, 1, 1,
-                0, 0.75f, 1, 1, 1, 1
+                -0.75f, -0.75f, 0,     1, 1, 1, 1,     0, 0,   0,
+                0.75f, -0.75f, 0,      1, 1, 1, 1,     1, 0,   0,
+                0, 0.75f, 0,           1, 1, 1, 1,     0.5f, 1,   0
         };
         mesh = new Mesh(shader, vertices.length, Buffer.BufferUsage.STATIC_DRAW);
         mesh.addVertices(vertices);
 
         window.setRenderer(this::drawTriangle);
+
+        glEnable(GL_DEPTH_TEST);
+        glDepthFunc(GL_LESS);
+
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
         window.loop();
 
         window.dispose();
         mesh.dispose();
         shader.dispose();
+        texture.dispose();
     }
 
     public static void main(String[] args) {
@@ -46,9 +63,14 @@ public class MeshTest {
     private void drawTriangle() {
         window.getSize(windowSize);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glViewport(windowSize.x, windowSize.y, 500, 500);
+        glViewport(0, 0, windowSize.x, windowSize.y);
         glClearColor(0, 0, 0, 1);
 
+        shader.uploadUniformInt("uTextures", 0);
+        texture.bindToSlot(0);
+
+        shader.uploadUniformMat4f("uView", camera.getView());
+        shader.uploadUniformMat4f("uProjection", camera.getProjection());
         mesh.draw(GL_TRIANGLES);
     }
 }

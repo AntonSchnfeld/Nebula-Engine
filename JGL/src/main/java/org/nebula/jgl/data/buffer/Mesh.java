4eb7c97325253835fadedb65ae20277d12ffdaf3
schoenfelddev <schnfeldanton@gmail.com>
2024-02-13 12:03:31 +0100

    Added JavaDoc to Mesh and changed Mesh.getVertices to return a ReadOnlyBuffer

--- JGL/src/main/java/org/nebula/jgl/data/buffer/Mesh.java
+++ JGL/src/main/java/org/nebula/jgl/data/buffer/Mesh.java
@@ -2,31 +2,52 @@ package org.nebula.jgl.data.buffer;
 
 import org.lwjgl.system.MemoryUtil;
 import org.nebula.base.interfaces.IDisposable;
-import org.nebula.jgl.JGL;
-import org.nebula.jgl.data.shader.Shader;
-import org.nebula.jgl.data.shader.VertexAttribs;
 import org.nebula.math.Transform;
 
 import java.nio.FloatBuffer;
 
-import static org.lwjgl.opengl.GL33C.glDrawArrays;
-
+/**
+ * <br>
+ * <h2>Mesh</h2>
+ * <br>
+ * The Mesh class represents a 3D mesh composed of vertices. It is designed to be used in conjunction
+ * with the JGL (Java Graphics Library) for handling OpenGL-related functionalities in Nebula Engine
+ * applications. The class provides methods to create and manipulate a mesh by specifying its vertices
+ * and transformation. The mesh can be rendered using OpenGL's glDrawArrays method.
+ *
+ * @author Anton Schoenfeld
+ */
 public class Mesh implements IDisposable {
 
     private FloatBuffer vertices;
     private Transform transform;
 
+    /**
+     * Constructs a Mesh object with the specified size for the vertex buffer.
+     *
+     * @param size The size of the vertex buffer.
+     */
     public Mesh(int size) {
         vertices = MemoryUtil.memAllocFloat(size);
         transform = new Transform();
     }
 
+    /**
+     * Constructs a Mesh object with the given array of vertices.
+     *
+     * @param vertices The array of vertices representing the mesh.
+     */
     public Mesh(float[] vertices) {
         this.vertices = MemoryUtil.memAllocFloat(vertices.length);
         this.vertices.put(0, vertices);
         transform = new Transform();
     }
 
+    /**
+     * Constructs a Mesh object with the specified FloatBuffer of vertices.
+     *
+     * @param vertices The FloatBuffer containing the vertices of the mesh.
+     */
     public Mesh(FloatBuffer vertices) {
         final int capacity = vertices.capacity();
         this.vertices = MemoryUtil.memAllocFloat(capacity);
@@ -34,32 +55,61 @@ public class Mesh implements IDisposable {
         transform = new Transform();
     }
 
+    /**
+     * Gets the transformation applied to the mesh.
+     *
+     * @return The transformation object associated with the mesh.
+     */
     public Transform getTransform() {
         return transform;
     }
 
+    /**
+     * Sets the transformation for the mesh.
+     *
+     * @param transform The new transformation for the mesh.
+     */
     public void setTransform(Transform transform) {
         this.transform = transform;
     }
 
+    /**
+     * Gets a read-only view of the mesh's vertices as a FloatBuffer.
+     *
+     * @return A read-only FloatBuffer containing the vertices of the mesh.
+     */
     public FloatBuffer getVertices() {
-        return vertices;
+        return vertices.asReadOnlyBuffer();
     }
 
+    /**
+     * Sets the vertices of the mesh using the provided FloatBuffer.
+     *
+     * @param vertices The new FloatBuffer containing the vertices of the mesh.
+     * @throws IllegalArgumentException If an indirect FloatBuffer is provided.
+     */
     public void setVertices(FloatBuffer vertices) {
         if (vertices == this.vertices) return;
         if (!vertices.isDirect())
             throw new IllegalArgumentException("Tried to use indirect FloatBuffer as vertices for mesh");
-        MemoryUtil.memFree(vertices);
+        MemoryUtil.memFree(this.vertices);
         this.vertices = vertices;
     }
 
+    /**
+     * Sets the vertices of the mesh using the provided array of floats.
+     *
+     * @param vertices The new array of floats representing the vertices of the mesh.
+     */
     public void setVertices(float[] vertices) {
         FloatBuffer buffer = MemoryUtil.memAllocFloat(vertices.length);
         buffer.put(0, vertices);
         setVertices(buffer);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -71,6 +121,9 @@ public class Mesh implements IDisposable {
         return transform.equals(mesh.transform);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int hashCode() {
         int result = vertices.hashCode();
@@ -78,6 +131,9 @@ public class Mesh implements IDisposable {
         return result;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String toString() {
         return "Mesh{" +
@@ -86,6 +142,9 @@ public class Mesh implements IDisposable {
                 '}';
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void dispose() {
         MemoryUtil.memFree(vertices);

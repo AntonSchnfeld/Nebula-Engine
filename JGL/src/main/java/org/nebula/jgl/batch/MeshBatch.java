b948b012131949fcb0cf3e9027b170e4db3e55a9
schoenfelddev <schnfeldanton@gmail.com>
2024-02-14 13:18:46 +0100

    Bugfixes

--- JGL/src/main/java/org/nebula/jgl/batch/MeshBatch.java
+++ JGL/src/main/java/org/nebula/jgl/batch/MeshBatch.java
@@ -16,9 +16,11 @@ import java.util.List;
 import static org.lwjgl.opengl.GL33C.*;
 
 public class MeshBatch extends Batch {
+    private static final int TRANSFORM_SIZE = 5;
     private final List<Mesh> meshes;
     private final VertexArray vertexArray;
     private final Buffer buffer;
+    private VertexAttribs vertexAttribs;
 
     public MeshBatch() {
         super();
@@ -30,8 +32,9 @@ public class MeshBatch extends Batch {
     @Override
     public void setShader(Shader shader) {
         super.setShader(shader);
+        this.vertexAttribs = shader.getVertexAttribs();
         buffer.bind();
-        shader.getVertexAttribs().configure(vertexArray);
+        vertexAttribs.configure(vertexArray);
     }
 
     @Override
@@ -43,17 +46,16 @@ public class MeshBatch extends Batch {
 
     @Override
     public void flush() {
-        final int len = calculateTotalVerticesSize();
-        FloatBuffer vertices = getBatchVertices(len);
+        final int totalVerticesSize = calculateTotalVerticesSize();
+        FloatBuffer vertices = getBatchVertices(totalVerticesSize);
 
         buffer.data(vertices, Buffer.Usage.STATIC_DRAW);
         vertexArray.bind();
-        final int count = len / shader.getVertexAttribs().getVertexSize();
+        final int totalCount = totalVerticesSize / vertexAttribs.getVertexSize();
         shader.bind();
         shader.uploadUniformMat4f("uView", viewMatrix);
         shader.uploadUniformMat4f("uProjection", projectionMatrix);
-        System.out.println(count);
-        glDrawArrays(GL_TRIANGLES, 0, count);
+        glDrawArrays(GL_TRIANGLES, 0, totalCount);
         JGL.checkForOpenGLError();
 
         MemoryUtil.memFree(vertices);
@@ -65,33 +67,55 @@ public class MeshBatch extends Batch {
         vertexArray.dispose();
     }
 
+    /**
+     * Returns the concatenated vertices of all stored Meshes in one native FloatBuffer. The transform values
+     * are inserted after each vertex. This assumes that the shader accepts these values.
+     *
+     * @param len the result of {@link MeshBatch#calculateTotalVerticesSize()}
+     * @return a native FloatBuffer containing the concatenated vertices of all stored meshes
+     */
     private FloatBuffer getBatchVertices(final int len) {
         FloatBuffer meshVertices = MemoryUtil.memAllocFloat(len);
-        int index = 0;
         for (Mesh mesh : meshes) {
-            FloatBuffer vertices = mesh.getVertices();
-            final int limit = vertices.limit();
-            meshVertices.put(index, vertices, 0, limit);
-            index += len;
+            final FloatBuffer vertices = mesh.getVertices();
+            final int count = vertices.limit() / (vertexAttribs.getVertexSize() - TRANSFORM_SIZE);
             final Transform transform = mesh.getTransform();
-            meshVertices.put(transform.getTranslation().x);
-            meshVertices.put(transform.getTranslation().y);
-            meshVertices.put(transform.getScale().x);
-            meshVertices.put(transform.getScale().y);
-            meshVertices.put(transform.getRotation());
-            index += 5;
+
+            // Add all the vertices of the current mesh into the native concatenated FloatBuffer
+            // and insert the transform values after each vertex.
+            // Vertex layout: {vertex data as defined by the shader}, translation, scale, rotation
+            for (int i = 0; i < count; i++) {
+
+                for (int j = 0; j < vertexAttribs.getVertexSize() - TRANSFORM_SIZE; j++) {
+                    meshVertices.put(vertices.get());
+                }
+
+                meshVertices.put(transform.getTranslation().x);
+                meshVertices.put(transform.getTranslation().y);
+                meshVertices.put(transform.getScale().x);
+                meshVertices.put(transform.getScale().y);
+                meshVertices.put(transform.getRotation());
+            }
         }
 
         return meshVertices;
     }
 
+    /**
+     * Calculates the total size of the vertices of all currently stored Meshes. In other words,
+     * the number of floats stored in each vertex in each mesh is combined.
+     *
+     * @return the total size of the vertices
+     */
     private int calculateTotalVerticesSize() {
         int len = 0;
-        VertexAttribs attribs = shader.getVertexAttribs();
         for (Mesh mesh : meshes) {
+            // Vertex buffer without transform values
             final FloatBuffer buffer = mesh.getVertices();
-            final int count = buffer.limit() / attribs.getVertexSize();
-            len += count * 5 + buffer.limit();
+            // Calculate count of vertices in the current Mesh
+            final int count = buffer.limit() / (vertexAttribs.getVertexSize() - TRANSFORM_SIZE);
+            // Calculate the amount of floats in the current mesh
+            len += count * vertexAttribs.getVertexSize();
         }
         return len;
     }

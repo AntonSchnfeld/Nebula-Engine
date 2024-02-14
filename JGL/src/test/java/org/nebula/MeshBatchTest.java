6ebd6dae593f8cc208d66b431d7ede33f513a40f
schoenfelddev <schnfeldanton@gmail.com>
2024-02-14 16:59:39 +0100

    Made the square rotate, move and scale using its transform

--- JGL/src/test/java/org/nebula/MeshBatchTest.java
+++ JGL/src/test/java/org/nebula/MeshBatchTest.java
@@ -1,12 +1,14 @@
 package org.nebula;
 
 import org.joml.Vector2f;
+import org.lwjgl.glfw.GLFW;
 import org.nebula.io.Files;
 import org.nebula.jgl.Camera;
 import org.nebula.jgl.batch.MeshBatch;
 import org.nebula.jgl.data.buffer.Mesh;
 import org.nebula.jgl.data.shader.Shader;
 import org.nebula.jglfw.GLFWWindow;
+import org.nebula.math.Maths;
 import org.nebula.math.Transform;
 
 public class MeshBatchTest {
@@ -15,7 +17,8 @@ public class MeshBatchTest {
     private final Camera camera;
 
     public MeshBatchTest() {
-        camera = new Camera();
+        final float size = 2;
+        camera = new Camera(new Vector2f(0, 0), size, -size, size, -size, size, -size);
         GLFWWindow window = new GLFWWindow(getClass().getName());
         window.createGLCapabilities();
         window.setWindowIcon(Files.readImageFromResource("images/nebula.png"));
@@ -24,13 +27,13 @@ public class MeshBatchTest {
                 Files.readResourceAsString("shaders/meshbatch/meshbatch.frag"));
         meshBatch = new MeshBatch();
         final float[] vertices = {
-                -0.75f, -0.75f, 1, 1, 1, 1, // Lower left
-                -0.75f, 0.75f, 1, 1, 1, 1, // Upper left
-                0.75f, 0.75f, 1, 1, 1, 1, // Upper right
+                -0.75f, -0.75f, 1, 0, 0, 1, // Lower left
+                -0.75f, 0.75f, 0, 1, 0, 1, // Upper left
+                0.75f, 0.75f, 0, 0, 1, 1, // Upper right
 
-                -0.75f, -0.75f, 1, 1, 1, 1, // Lower left
-                0.75f, -0.75f, 1, 1, 1, 1, // Lower right
-                0.75f, 0.75f, 1, 1, 1, 1, // Upper right
+                -0.75f, -0.75f, 1, 0, 0, 1, // Lower left
+                0.75f, -0.75f, 1, 0, 1, 1, // Lower right
+                0.75f, 0.75f, 0, 0, 1, 1, // Upper right
         };
         mesh = new Mesh(vertices);
         Transform transform = mesh.getTransform();
@@ -57,6 +60,10 @@ public class MeshBatchTest {
         meshBatch.setViewMatrix(camera.getView());
         meshBatch.setProjectionMatrix(camera.getProjection());
         meshBatch.begin();
+        final Transform transform = mesh.getTransform();
+        transform.setRotation(transform.getRotation() + 1f);
+        transform.getScale().set(Math.sin(GLFW.glfwGetTime()), Math.sin(GLFW.glfwGetTime()));
+        transform.getTranslation().set(Math.sin(GLFW.glfwGetTime()), Math.sin(GLFW.glfwGetTime()));
         meshBatch.mesh(mesh);
         meshBatch.end();
     }
